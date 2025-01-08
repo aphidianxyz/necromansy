92667adbce24fe22a4ddbe2bc53e49ac7c388e46
Dihsar <ackrashid@gmail.com>
2025-01-07 23:52:16 -0500

    added unit stats

---
+++ Assets/Scripts/unitStats.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class unitStats : MonoBehaviour 
+{
+    public float health = 30f;
+    public float damage = 5f;
+    public float attackRange = 4f;
+    public float moveSpeed = 2f;
+    bool isRanged = false;
+    public float cost = 2f;
+    public float bloodLevel = 0f;
+
+    
+}
