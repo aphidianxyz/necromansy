b177fac77ea88b9c222fb1d968f85a6f7ce12205
Dihsar <ackrashid@gmail.com>
2025-01-08 21:56:13 -0500

    Started working on attack logic

---
+++ Assets/Scripts/unitAttackLogic.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class unitAttackLogic : MonoBehaviour
+{
+    public CircleCollider2D attackRangeCircle;
+
+	public unitStats unitStats;
+	private float attackTimer = 0f;
+
+	private string opposingTag = "Enemy";
+	private GameObject target;
+
+	private List<Collider2D> colisions = new List<Collider2D>{};
+	private void Start()
+	{
+		if (gameObject.CompareTag("Enemy")) {
+
+			opposingTag = "Player";
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		
+		if (!collision.isTrigger && collision.CompareTag(opposingTag))
+		{
+			target = collision.gameObject;
+			Debug.Log(target);
+		}
+	}
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		
+		if (collision.gameObject == target)
+		{
+			attackTimer += Time.deltaTime;
+			if (attackTimer >= unitStats.timeBetweenAttacks)
+			{
+				target.GetComponent<unitStats>().TakeDamage(unitStats.damage);
+				attackTimer = 0f;
+				Debug.Log("Attack");
+			}
+		}
+	}
+
+}
--- Assets/Scripts/unitStats.cs
+++ Assets/Scripts/unitStats.cs
@@ -3,15 +3,30 @@ using UnityEngine;
 public class unitStats : MonoBehaviour 
 {
     public float health = 30f;
+
     public float damage = 5f;
     public float attackRange = 4f;
-    public float moveSpeed = 2f;
-    bool isRanged = false;
+    public float timeBetweenAttacks = 2f;
+	bool isRanged = false;
+
+	public float moveSpeed = 2f;
+    
     public float cost = 2f;
     public float bloodLevel = 0f;
 
     void Start()
     {
-        gameObject.GetComponentInChildren<SphereCollider>().radius = attackRange;
+        gameObject.GetComponents<CircleCollider2D>()[1].radius = attackRange;
+    }
+
+	private void Update()
+	{
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+	}
+
+    public void TakeDamage(float damageDelt) { 
+        health -= damageDelt;
     }
 }
