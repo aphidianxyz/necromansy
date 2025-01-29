1e988e02959d999e0aaf165789657b581430ef1a
aphidianxyz <github@aphidian.aleeas.com>
2025-01-28 19:44:37 -0500

    Adjust vertical cell size of tilemaps to be slightly less than 1 to get rid of vertical line artifact, implement a scenemanager that turns on navmesh on game start so that the editor isn't obscured by it

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject navmesh;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        navmesh.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
